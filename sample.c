168c37522d1cd86f43565da6c60a757915bfa818
vincent040 <18620373040@163.com>
2019-04-01 22:54:09 +0900

    代码模板

---
+++ sample.c
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////
+//
+//  Copyright(C), 广州粤嵌通信科技股份有限公司
+//
+//  作者: Vincent Lin (林世霖)
+//
+//  微信公众号: 秘籍酷
+//  技术交流群: 260492823（QQ群）
+//  GitHub链接: https://github.com/vincent040
+//
+//  描述:
+//
+////////////////////////////////////////////////
+
+#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
